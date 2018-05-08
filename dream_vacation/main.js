84ebc437493dbc5e634d7f79d76292ee5ed08282
YOUR NAME <YOUR EMAIL ADDRESS>
2018-05-08 17:58:03 -0400

    new

--- dream_vacation/main.js
+++ dream_vacation/main.js
@@ -3,14 +3,19 @@ var videoSources = [
   'video2.mp4',
   'video3.mp4',
   'video4.mp4',
-  'video4.mp7',
-  'video4.mp8',
-  'video4.mp9',
-  'video4.mp10',
-  'video4.mp11',
-  'video4.mp12',
-  'video4.mp13',
-  'video4.mp14'
+  'video5.mp4',
+  'video6.mp4',
+  'video7.mp4',
+  'video8.mp4',
+  'video9.mp4',
+  'video10.mp4',
+  'video11.mp4',
+  'video12.mp4',
+  'video13.mp4',
+  'video14.mp4',
+  'video15.mp4',
+  'video16.mp4',
+  'video17.mp4'
 ];
 
 var getRandomVideoSrc = function (){
@@ -23,4 +28,11 @@ var video = $('.video-wrapper-1 video');
 
 video.on('ended', function() {
   $('.video-wrapper-1 video source').attr( 'src', getRandomVideoSrc() );
+});
+
+$(function() {
+  $('video').each(function() {
+    var video = $(this).get(0);
+    video.play();
+  });
 });
