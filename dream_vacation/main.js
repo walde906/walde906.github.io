1643fa3161a86b64d6a4619facd83837bb6ea934
YOUR NAME <YOUR EMAIL ADDRESS>
2018-05-10 14:19:18 -0400

    links

--- dream_vacation/main.js
+++ dream_vacation/main.js
@@ -46,22 +46,66 @@ $(document).on('keypress', function(e) {
 
   console.log('keypress:', key);
 
-  if ( key == 'v' ) {
-    img = '.video-wrapper-5("style","opacity:1.0; -moz-opacity:1.0; filter:alpha(opacity=100)"';
+  if ( key == 'a' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/eight.png';
   } else if ( key == 'b' ) {
-    img = 'vid8.mp4'("style","top: 4%;","left: 49%;","position: absolute;","width: 52%;","height: 46%;");
-  } else if ( key == 'n' ) {
-    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/test.png';
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/eighteen.png';
+  } else if ( key == 'c' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/eleven.png';
+  }  else if ( key == 'd' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/fifteen.png';
+  }  else if ( key == 'e' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/five.png';
+  }  else if ( key == 'f' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/four.png';
+  }  else if ( key == 'g' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/fourteen.png';
+  }  else if ( key == 'h' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/nine.png';
+  }  else if ( key == 'i' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/nineteen.png';
+  }  else if ( key == 'j' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/one.png';
+  }  else if ( key == 'k' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/seven.png';
+  }  else if ( key == 'l' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/seventeen.png';
   }  else if ( key == 'm' ) {
-    img = 'http://www.pngmart.com/files/1/Mango-Fruit-PNG.png';
-  } else if (key == 'c') {
-  	img = 'http://www.pngmart.com/files/1/Piano-Clip-Art-PNG.png'
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/six.png';
+  }  else if ( key == 'n' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/sixteen.png';
+  }  else if ( key == 'o' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/ten.png';
+  }  else if ( key == 'p' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/thirteen.png';
+  }  else if ( key == 'q' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/three.png';
+  }  else if ( key == 'r' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twelve.png';
+  }  else if ( key == 's' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twenty.png';
+  }  else if ( key == 't' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twentyfive.png';
+  }  else if ( key == 'u' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twentyfour.png';
+  }  else if ( key == 'v' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twentyone.png';
+  }  else if ( key == 'w' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twentysix.png';
+  }  else if ( key == 'x' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twentythree.png';
+  }  else if ( key == 'y' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/twentytwo.png';
+  }  else if ( key == 'z' ) {
+    img = 'file:///Users/emilwaldron/walde906.github.io/dream_vacation/assets/two.png';
   }
 
-    if ( img ) {
-    $('body').append('<img src="' + img + '"style="position: fixed; top: 4%; left: 49%; max-width: 52%;">');
-  } else {
-    $('body').append('<img src="' + img + '"style="position: fixed; top: 5%; left: -2%; max-width: 52%;">');  }
-});
 
+
+  if ( img ) {
+    $('body').append('<img src="' + img + '" style="position: fixed; top: '+ Math.random() * 100 + '%; left: ' + Math.random() * 100 + '%; transform: translate(-50%, -50%); max-width: 300px;">');
+  } else {
+    $('body').append('<h2 style="position: fixed; top: '+ Math.random() * 100 + '%; left: ' + Math.random() * 100 + '%; transform: translate(-50%, -50%); font-size: ' + Math.random() * 200 + 'px;">' + key + '</h2>');
+  }
+});
 
